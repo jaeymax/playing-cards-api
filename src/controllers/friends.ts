c0e3b378666bf4d1c87cfc6141626565e7a1c6be
jaeymax <azagojunior2@gmail.com>
2025-02-21 23:03:56 +0000

    updated deploy.yml

--- src/controllers/friends.ts
+++ src/controllers/friends.ts
@@ -3,7 +3,23 @@ import { Request, Response } from "express";
 
 const getFriends = async(req:Request, res:Response)=>{
 
+
+    const friends = [
+        {
+            id:1,
+            name:"virgil"
+        },
+        {
+            id:2,
+            name:"maxwell"
+        }
+    ]
     res.json({message:"get Friends controller"});
+    //const friends = 'select ( frp m'
+
+    console.log(friends);
+    
+
 }
 
 const addFriend = async(req:Request, res:Response) =>{
