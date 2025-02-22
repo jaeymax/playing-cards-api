b01fa83e38f3218ced69ddbb95e6f2cba9225f0e
jaeymax <azagojunior2@gmail.com>
2025-02-22 00:03:55 +0000

    edited authService.test.ts

--- tests/unit/authService.test.ts
+++ tests/unit/authService.test.ts
@@ -4,6 +4,6 @@ import {describe, it, expect} from '@jest/globals';
 describe('authService', ()=>{
     it('should return true', ()=>{
         
-        expect(20 > 3).toBe(false);
+        expect(20 > 5).toBe(false);
     })
 })
